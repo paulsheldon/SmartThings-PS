ae279de6d880eaa52f904ca804f39b1e3ce69c7e
paulsheldon1966 <paul@utrak.co.uk>
2019-11-21 12:14:31 +0000

    UPdated file names to work with 'repo updating'

--- devicetypes/paulsheldon/zigbee-switch-power.src/zigbee-switch-power.groovy
+++ devicetypes/paulsheldon/zigbee-switch-power.src/zigbee-switch-power.groovy
@@ -10,10 +10,11 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * Updated to add Salus SP600 Smart Plug
  */
 
 metadata {
-    definition (name: "ZigBee Switch Power", namespace: "smartthings", author: "SmartThings", ocfDeviceType: "oic.d.switch", runLocally: false, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true) {
+    definition (name: "ZigBee Switch Power", namespace: "paulsheldon", author: "SmartThings", ocfDeviceType: "oic.d.switch", runLocally: false, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true) {
         capability "Actuator"
         capability "Configuration"
         capability "Refresh"
